668937e55f3e29210696a7105fb6a23e9ed50fe2
ototonho <antonio.steffen@ufpr.br>
2025-10-14 17:38:54 -0300

    .

---
+++ testes/comentários e coisas que usei.R
@@ -0,0 +1,14 @@
+# Comentários e coisas que usei
+
+# reticulate::repl_python()
+# reticulate::py_to_r()
+# reticulate::py_install("numpy")
+# reticulate::py_install("pandas")
+
+# library(usethis)
+# usethis::create_from_github()
+
+# library(usethis)
+# use_git_config(user.name = "ototonho",
+#               user.email = "antonio.steffen@ufpr.br")
+# gitcreds::gitcreds_set()
